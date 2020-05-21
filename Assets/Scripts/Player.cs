f973eb3dc5d55dd815241069126efbda4b6d221a
Mark Strefford <markstrefford@gmail.com>
2020-05-21 22:05:17 +0100

    Initial commit (p22)

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 3.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // take the current position = new position (0, 0, 0)
+        transform.position = new Vector3(0, 0, 0);
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        // transform.Translate(Vector3.right * horizontalInput * _speed * Time.deltaTime);
+        // transform.Translate(Vector3.up * verticalInput * _speed * Time.deltaTime);
+         
+        transform.Translate(new Vector3(horizontalInput, verticalInput, 0) * _speed * Time.deltaTime);
+    }
+}
