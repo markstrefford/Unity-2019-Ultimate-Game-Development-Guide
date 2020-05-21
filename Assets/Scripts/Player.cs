b0081f2655b1501e10ff70de31df1259e05fcb17
Mark Strefford <markstrefford@gmail.com>
2020-05-21 23:04:12 +0100

    End of module 4 - Create the player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -12,18 +12,25 @@ public class Player : MonoBehaviour
     {
         // take the current position = new position (0, 0, 0)
         transform.position = new Vector3(0, 0, 0);
-    
     }
 
     // Update is called once per frame
     void Update()
     {
+        CalculateMovement();
+    }
+
+    void CalculateMovement()
+    {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
-        // transform.Translate(Vector3.right * horizontalInput * _speed * Time.deltaTime);
-        // transform.Translate(Vector3.up * verticalInput * _speed * Time.deltaTime);
-         
-        transform.Translate(new Vector3(horizontalInput, verticalInput, 0) * _speed * Time.deltaTime);
+        Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
+        transform.Translate(direction * _speed * Time.deltaTime);
+
+        // Video 25 wraps player from one side to the other at -11.3 / +11.3
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, -9f, 9f), 
+                                         Mathf.Clamp(transform.position.y, -3.8f, 0), 
+                                         0); 
     }
 }
